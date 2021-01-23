f208089ef3c16498add442051acd110206197293
Thomas McCausland <2472525m@student.gla.ac.uk>
2021-01-23 16:51:21 +0100

    Chapter 4: implemented template for index page and added static and media files

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! Here you'll find the <a href='/rango/about/'>About</a> page.")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+
 
 def about(request):
     return HttpResponse("Rango says here is the about page. Here you'll find the <a href='/rango/'>Index</a> page.")
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -15,6 +15,14 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# The directory containing all the templates used in each app
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
+# The directory containing the static files used
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+# The directory containging any media files that are used
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -55,10 +63,11 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.media',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
@@ -119,3 +128,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [STATIC_DIR, ]
+
+
+# Media files
+
+MEDIA_ROOT = MEDIA_DIR
+
+MEDIA_URL = '/media/'
