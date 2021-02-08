dfac7f6e77c51501e6c436940a21ea7005e32671
Thomas McCausland <2472525m@student.gla.ac.uk>
2021-02-08 13:45:21 +0100

    Chapter 10: implemented client-side cookies and used them to count site visits

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.urls import reverse
+from datetime import datetime
+
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
-from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
+from rango.models import Category, Page
+
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -15,7 +18,13 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = pages_list
-    return render(request, 'rango/index.html', context=context_dict)
+    context_dict['visits'] = int(request.COOKIES.get('visits', '1'))
+
+    response = render(request, 'rango/index.html', context=context_dict)
+
+    visitor_cookie_handler(request, response)
+
+    return response
 
 
 def about(request):
@@ -121,3 +130,17 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
+
+def visitor_cookie_handler(request, response):
+    visits = int(request.COOKIES.get('visits', '1'))
+
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        response.set_cookie('last_visit', str(datetime.now()))
+    else:
+        response.set_cookie('last_visit', last_visit_cookie)
+
+    response.set_cookie('visits', visits)
